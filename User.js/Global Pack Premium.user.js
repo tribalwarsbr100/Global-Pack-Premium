659c17c32896b2bd51c21c735e7a0201bcea1b55
TW 100 <tribalwarsbr100@gmail.com>
2018-03-29 13:29:21 -0300

    Create Global Pack Premium.user.js

---
+++ User.js/Global Pack Premium.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name                Global Pack Premium Tribal Wars
+// @namespace           @@marcosvinicius.santosmarques
+// @icon                https://i.imgur.com/7WgHTT8.gif
+// @website             https://tribalwarsbr100.wixsite.com/tw100
+// @email               tribalwarsbr100@gmail.com
+// @description 	    Ferramenta simulatoria do conteudo conhecido como premium, no game tribal wars, com diversas funções em javascript do qual compoem o Pack Premium
+// @codigo              Conteudo feito em linguagem javascript com base em EcmaScript totalmente Opensource
+// @author		        Marcos v.s Marques
+// @include             http*://*.*game.php*
+// @version     	    0.0.1
+// @updateURL		    dfdf
+// @downloadURL         dfdf
+// @supportURL          dfdf
+// @grant               GM_getResourceText
+// @grant               GM_addStyle
+// @grant               GM_getValue
+// @grant               unsafeWindow
+// ==/UserScript==
